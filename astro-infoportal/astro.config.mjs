797703d58a4d1ef5704f560433d4e4b32aed46e8
Pooria Mehregan <pooriamehregan@gmail.com>
2026-03-05 14:47:20 +0100

    feat: add astro react integration

--- astro-infoportal/astro.config.mjs
+++ astro-infoportal/astro.config.mjs
@@ -3,15 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
-    i18n: {
-    locales: ["nb", "nn", "en"],
-    defaultLocale: "nb",
-        routing: {
-        prefixDefaultLocale: false
-    }
+
+  i18n: {
+  locales: ["nb", "nn", "en"],
+  defaultLocale: "nb",
+      routing: {
+      prefixDefaultLocale: false
   }
+},
+
+  integrations: [react(
+    { experimentalReactChildren: true }
+  )]
 });
