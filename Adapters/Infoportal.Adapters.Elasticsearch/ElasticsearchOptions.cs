e263fe31b5ab170d42e88a618b9215fcdaca3933
Quan Vu <quan.vu@knowit.no>
2026-03-26 16:25:50 +0100

    Removed fallback values

--- Adapters/Infoportal.Adapters.Elasticsearch/ElasticsearchOptions.cs
+++ Adapters/Infoportal.Adapters.Elasticsearch/ElasticsearchOptions.cs
@@ -4,8 +4,8 @@ public class ElasticsearchOptions
 {
     // TODO: In production, override Url and ApiKey via deployment.yaml env vars:
     // ElasticsearchOptions__Url and ElasticsearchOptions__ApiKey (FluxCD substitution)
-    public string Url { get; set; } = "http://localhost:9200";
-    public string IndexPrefix { get; set; } = "infoportal";
+    public string Url { get; set; } = "";
+    public string IndexPrefix { get; set; } = "";
     public string? ApiKey { get; set; }
     public int PageSize { get; set; } = 10;
 
