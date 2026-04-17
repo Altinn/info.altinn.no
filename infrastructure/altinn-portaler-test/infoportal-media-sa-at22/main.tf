022f2fcca03bbd27217887bb2b47f068a6668724
Bengt Fredh <32734566+bengtfredh@users.noreply.github.com>
2026-04-17 15:10:27 +0200

    feat: Add infoportal media storage account for AT22
    Infrastructure as code for provisioning Azure storage account
    and associated resources for infoportal media in the AT22 test
    environment, including GitHub Actions workflow for automated
    deployment.

---
+++ infrastructure/altinn-portaler-test/infoportal-media-sa-at22/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "main" {
+  name     = "infoportal-media-sa-at22"
+  location = "Norway East"
+  tags     = local.tags
+}
+
+locals {
+  tags = {
+    finops_environment = "at22"
+    finops_product     = "infoportal"
+    repository         = "https://github.com/Altinn/info.altinn.no"
+    env                = "at22"
+    product            = "infoportal"
+  }
+}
+
+module "media_sa" {
+  source = "../../modules/infoportal-media-sa"
+
+  environment          = "at22"
+  location             = azurerm_resource_group.main.location
+  resource_group_name  = azurerm_resource_group.main.name
+  storage_account_base_name = "infoportalmediaat22"
+  umbraco_sp_object_id = var.umbraco_sp_object_id
+  tags                 = local.tags
+}
