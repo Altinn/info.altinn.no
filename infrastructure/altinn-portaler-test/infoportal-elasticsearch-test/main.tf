eb5bd34c3113af34c862d55f1661efccc7960523
Bengt Fredh <32734566+bengtfredh@users.noreply.github.com>
2026-04-17 14:11:08 +0200

    Align Terraform variable assignments in infoportal elasticsearch test config

--- infrastructure/altinn-portaler-test/infoportal-elasticsearch-test/main.tf
+++ infrastructure/altinn-portaler-test/infoportal-elasticsearch-test/main.tf
@@ -8,22 +8,22 @@ resource "azurerm_resource_group" "main" {
 
 locals {
   tags = {
-    finops_environment       = "tt02"
-    finops_product           = "infoportal"
-    repository               = "https://github.com/Altinn/info.altinn.no"
-    env                      = "tt02"
-    product                  = "infoportal"
+    finops_environment = "tt02"
+    finops_product     = "infoportal"
+    repository         = "https://github.com/Altinn/info.altinn.no"
+    env                = "tt02"
+    product            = "infoportal"
   }
 }
 
 module "elasticsearch" {
   source = "../../modules/infoportal-elasticsearch"
 
-  environment           = "tt02"
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  tenant_id             = data.azurerm_client_config.current.tenant_id
-  terraform_object_id   = data.azurerm_client_config.current.object_id
+  environment                 = "tt02"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  terraform_object_id         = data.azurerm_client_config.current.object_id
   eso_principal_id            = var.eso_principal_id
   elastic_cloud_api_key       = var.elastic_cloud_api_key
   elastic_cloud_email_address = var.elastic_cloud_email_address
