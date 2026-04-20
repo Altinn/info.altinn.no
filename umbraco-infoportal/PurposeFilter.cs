90d5e4425dbe760ab9c6b5e6fd33dd859e17e424
Alv G. Lia <alv.gullbrand.lia@digdir.no>
2026-04-20 09:19:43 +0200

    feat:Added Content Delivery API support for relations

---
+++ umbraco-infoportal/PurposeFilter.cs
@@ -0,0 +1,6 @@
+using Umbraco.Cms.Core.DeliveryApi;
+
+public class PurposeFilter : AbstractRelationFilter, IFilterHandler, IContentIndexHandler
+{
+    public PurposeFilter() : base("purpose", "purposes", ["purposePage"]) {}
+}
