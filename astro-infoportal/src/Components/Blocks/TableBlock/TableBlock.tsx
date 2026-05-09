Altinn/info.altinn.no

commit a5609603345331e5c7f002e550251e9d9c10ed1e

feat: start-and-run parity for guides, news, documents and contact forms (#160)

astro-infoportal/src/transformers/HeroArticlePageBaseTransformer.ts
@@ -1,39 +1,87 @@
-import { type Locale, t } from "@i18n/index";
-import { fetchUmbracoAncestors } from "../api/umbraco/client";
-import { BreadcrumbsTransformer } from "./BreadcrumbsTransformer";
 import type { IJSONTransformer } from "./IJSONTransformer";
+import { fetchUmbracoAncestors, fetchUmbracoContent } from "../api/umbraco/client";
+import { BlockTransformer } from "./BlockTransformer";
+import { BreadcrumbsTransformer } from "./BreadcrumbsTransformer";
+import { type Locale, t } from "@i18n/index";
 
-function formatDate(dateStr: string): string {
-  const d = new Date(dateStr);
-  if (Number.isNaN(d.getTime())) return "";
-  const day = String(d.getDate()).padStart(2, "0");
-  const month = String(d.getMonth() + 1).padStart(2, "0");
-  return `${day}.${month}.${d.getFullYear()}`;
+function formatDate(dateStr?: string): string | undefined {
+  if (!dateStr) return undefined;
+  const date = new Date(dateStr);
+  if (Number.isNaN(date.getTime())) return undefined;
+
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  return `${day}.${month}.${date.getFullYear()}`;
+}
+
+function normalizeRichTextHtml(html?: string): string {
+  if (!html) {
+    return "";
+  }
+
+  return html.replace(
+    /((?:href|src)=["'])(\/dokumentmaler\/[^"']+)(["'])/gi,
+    "$1/globalassets$2$3",
+  );
+}
+
+function toRichTextArea(value: any) {
+  if (!value?.items?.length) {
+    return undefined;
+  }
+
+  return {
+    items: value.items.map((item: any) => ({
+      componentName: item.componentName ?? "RichText",
+      html: normalizeRichTextHtml(item.html),
+    })),
+  };
+}
+
+async function hydrateContentAreaItems(items: any, locale: Locale) {
+  if (!Array.isArray(items)) {
+    return undefined;
+  }
+
+  const hydratedItems = await Promise.all(
+    items.map(async (item: any) => {
+      if (!item?.route?.path && !item?.id) {
+        return item;
+      }
+
+      try {
+        return await fetchUmbracoContent(item.id ?? item.route.path, locale);
+      } catch {
+        return item;
+      }
+    }),
+  );
+
+  return BlockTransformer.TransformBlocks(hydratedItems);
 }
 
 export class HeroArticlePageBaseTransformer implements IJSONTransformer {
   public async Transform(cmsPageData: any, globalData?: any): Promise<any> {
-    const ancestors = await fetchUmbracoAncestors(cmsPageData.route.path);
+    const locale: Locale = globalData?.locale || "nb";
+    const props = cmsPageData.properties ?? {};
+    const ancestors = await fetchUmbracoAncestors(cmsPageData.route.path, locale);
     const breadcrumb = BreadcrumbsTransformer.Transform(ancestors, cmsPageData);
-
-    const mainBody = cmsPageData.properties.mainBody;
-    const locale: Locale = globalData?.locale ?? "nb";
-    const lastUpdatedDateString = formatDate(cmsPageData.updateDate ?? "");
-    const lastUpdatedDateText = lastUpdatedDateString
-      ? t("common.lastUpdated", locale)
+    const mainBody = toRichTextArea(props.mainBody);
+    const bottomContentArea = props.bottomContentArea
+      ? await hydrateContentAreaItems(props.bottomContentArea, locale)
       : undefined;
+    const lastUpdatedDateString = formatDate(props.lastChanged ?? cmsPageData.updateDate);
 
     return {
       componentName: "HeroArticlePageBase",
       pageName: cmsPageData.name,
       articlePageHero: {},
-      mainIntro: cmsPageData.properties.mainIntro,
+      mainIntro: props.mainIntro,
       mainBody: mainBody ? { ...mainBody, addAnchors: true } : mainBody,
-      breadcrumb: breadcrumb,
-      timeline: cmsPageData.properties.timeline ?? [],
-      bottomContentArea: cmsPageData.properties.bottomContentArea ?? undefined,
-      lastUpdatedDateText,
-      lastUpdatedDateString: lastUpdatedDateString || undefined,
+      breadcrumb,
+      lastUpdatedDateText: lastUpdatedDateString ? t("common.lastUpdated", locale) : undefined,
+      lastUpdatedDateString,
+      bottomContentArea,
       isUserLoggedIn: false,
     };
   }

astro-infoportal/public/assets/astro.svg
@@ -1,1 +1,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="115" height="48"><path fill="#17191E" d="M7.77 36.35C6.4 35.11 6 32.51 6.57 30.62c.99 1.2 2.35 1.57 3.75 1.78 2.18.33 4.31.2 6.33-.78.23-.12.44-.27.7-.42.18.55.23 1.1.17 1.67a4.56 4.56 0 0 1-1.94 3.23c-.43.32-.9.61-1.34.91-1.38.94-1.76 2.03-1.24 3.62l.05.17a3.63 3.63 0 0 1-1.6-1.38 3.87 3.87 0 0 1-.63-2.1c0-.37 0-.74-.05-1.1-.13-.9-.55-1.3-1.33-1.32a1.56 1.56 0 0 0-1.63 1.26c0 .06-.03.12-.05.2Z"/><path fill="url(#a)" d="M7.77 36.35C6.4 35.11 6 32.51 6.57 30.62c.99 1.2 2.35 1.57 3.75 1.78 2.18.33 4.31.2 6.33-.78.23-.12.44-.27.7-.42.18.55.23 1.1.17 1.67a4.56 4.56 0 0 1-1.94 3.23c-.43.32-.9.61-1.34.91-1.38.94-1.76 2.03-1.24 3.62l.05.17a3.63 3.63 0 0 1-1.6-1.38 3.87 3.87 0 0 1-.63-2.1c0-.37 0-.74-.05-1.1-.13-.9-.55-1.3-1.33-1.32a1.56 1.56 0 0 0-1.63 1.26c0 .06-.03.12-.05.2Z"/><path fill="#17191E" d="M.02 30.31s4.02-1.95 8.05-1.95l3.04-9.4c.11-.45.44-.76.82-.76.37 0 .7.31.82.76l3.04 9.4c4.77 0 8.05 1.95 8.05 1.95L17 11.71c-.2-.56-.53-.91-.98-.91H7.83c-.44 0-.76.35-.97.9L.02 30.31Zm42.37-5.97c0 1.64-2.05 2.62-4.88 2.62-1.85 0-2.5-.45-2.5-1.41 0-1 .8-1.49 2.65-1.49 1.67 0 3.09.03 4.73.23v.05Zm.03-2.04a21.37 21.37 0 0 0-4.37-.36c-5.32 0-7.82 1.25-7.82 4.18 0 3.04 1.71 4.2 5.68 4.2 3.35 0 5.63-.84 6.46-2.92h.14c-.03.5-.05 1-.05 1.4 0 1.07.18 1.16 1.06 1.16h4.15a16.9 16.9 0 0 1-.36-4c0-1.67.06-2.93.06-4.62 0-3.45-2.07-5.64-8.56-5.64-2.8 0-5.9.48-8.26 1.19.22.93.54 2.83.7 4.06 2.04-.96 4.95-1.37 7.2-1.37 3.11 0 3.97.71 3.97 2.15v.57Zm11.37 3c-.56.07-1.33.07-2.12.07-.83 0-1.6-.03-2.12-.1l-.02.58c0 2.85 1.87 4.52 8.45 4.52 6.2 0 8.2-1.64 8.2-4.55 0-2.74-1.33-4.09-7.2-4.39-4.58-.2-4.99-.7-4.99-1.28 0-.66.59-1 3.65-1 3.18 0 4.03.43 4.03 1.35v.2a46.13 46.13 0 0 1 4.24.03l.02-.55c0-3.36-2.8-4.46-8.2-4.46-6.08 0-8.13 1.49-8.13 4.39 0 2.6 1.64 4.23 7.48 4.48 4.3.14 4.77.62 4.77 1.28 0 .7-.7 1.03-3.71 1.03-3.47 0-4.35-.48-4.35-1.47v-.13Zm19.82-12.05a17.5 17.5 0 0 1-6.24 3.48c.03.84.03 2.4.03 3.24l1.5.02c-.02 1.63-.04 3.6-.04 4.9 0 3.04 1.6 5.32 6.58 5.32 2.1 0 3.5-.23 5.23-.6a43.77 43.77 0 0 1-.46-4.13c-1.03.34-2.34.53-3.78.53-2 0-2.82-.55-2.82-2.13 0-1.37 0-2.65.03-3.84 2.57.02 5.13.07 6.64.11-.02-1.18.03-2.9.1-4.04-2.2.04-4.65.07-6.68.07l.07-2.93h-.16Zm13.46 6.04a767.33 767.33 0 0 1 .07-3.18H82.6c.07 1.96.07 3.98.07 6.92 0 2.95-.03 4.99-.07 6.93h5.18c-.09-1.37-.11-3.68-.11-5.65 0-3.1 1.26-4 4.12-4 1.33 0 2.28.16 3.1.46.03-1.16.26-3.43.4-4.43-.86-.25-1.81-.41-2.96-.41-2.46-.03-4.26.98-5.1 3.38l-.17-.02Zm22.55 3.65c0 2.5-1.8 3.66-4.64 3.66-2.81 0-4.61-1.1-4.61-3.66s1.82-3.52 4.61-3.52c2.82 0 4.64 1.03 4.64 3.52Zm4.71-.11c0-4.96-3.87-7.18-9.35-7.18-5.5 0-9.23 2.22-9.23 7.18 0 4.94 3.49 7.59 9.21 7.59 5.77 0 9.37-2.65 9.37-7.6Z"/><defs><linearGradient id="a" x1="6.33" x2="19.43" y1="40.8" y2="34.6" gradientUnits="userSpaceOnUse"><stop stop-color="#D83333"/><stop offset="1" stop-color="#F041FF"/></linearGradient></defs></svg>

astro-infoportal/public/assets/img/a-logo-black.svg
@@ -1,0 +1,9 @@
+<svg width="117" height="29" viewBox="0 0 117 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M14.8827 0H0.587321C0.431554 0 0.282167 0.0617493 0.172022 0.171663C0.0618784 0.281578 0 0.430653 0 0.586095V28.4139C0 28.5693 0.0618784 28.7184 0.172022 28.8283C0.282167 28.9383 0.431554 29 0.587321 29H14.8827C18.6633 28.8927 22.253 27.3186 24.8891 24.6121C27.5252 21.9056 29 18.2801 29 14.5059C29 10.7316 27.5252 7.10608 24.8891 4.39961C22.253 1.69314 18.6633 0.119023 14.8827 0.0117222V0ZM16.2805 24.6277C14.1176 24.9992 11.8924 24.674 9.92698 23.6993C7.9616 22.7246 6.35811 21.1509 5.3487 19.2062C4.33929 17.2614 3.97636 15.0465 4.31245 12.8822C4.64854 10.7179 5.66622 8.71655 7.21812 7.16789C8.77002 5.61923 10.7756 4.60368 12.9444 4.26828C15.1132 3.93289 17.3328 4.29507 19.2816 5.30237C21.2304 6.30967 22.8073 7.90981 23.7841 9.87109C24.7609 11.8324 25.0867 14.053 24.7145 16.2114C24.3522 18.3119 23.3469 20.2488 21.8366 21.7559C20.3264 23.263 18.3854 24.2662 16.2805 24.6277Z" fill="black"/>
+<path d="M48.6302 4.21977H52.5183C52.5836 4.20181 52.6526 4.20181 52.718 4.21977L59.5426 24.569C59.5426 24.7214 59.5426 24.8034 59.3899 24.8034H56.3124C56.2536 24.8034 56.1949 24.8034 56.1597 24.6628L54.8206 20.4663H45.7758C45.7381 20.4667 45.701 20.4558 45.6696 20.4349C45.6382 20.4139 45.6138 20.3841 45.5996 20.3491L44.2371 24.5573C44.2371 24.7214 44.194 24.8034 44.1078 24.8034H41.6763C41.551 24.8034 41.5001 24.7566 41.5236 24.6628L48.454 4.31355C48.4726 4.28386 48.4986 4.25957 48.5295 4.24311C48.5605 4.22664 48.5952 4.2186 48.6302 4.21977ZM46.6098 18.0282H53.8926C54.0101 18.0282 54.0492 17.9774 54.0101 17.8758L50.5801 7.15025C50.5573 7.08553 50.5141 7.02996 50.4569 6.9919C50.3996 6.95385 50.3316 6.93537 50.263 6.93926C50.1103 6.93926 50.0163 6.93926 49.9693 7.05648L46.4454 17.7703C46.4297 17.9422 46.4845 18.0282 46.6098 18.0282Z" fill="black"/>
+<path d="M65.1808 4.36036V24.7096C65.1826 24.7285 65.1802 24.7476 65.1737 24.7655C65.1673 24.7834 65.157 24.7996 65.1435 24.8131C65.13 24.8265 65.1138 24.8368 65.0958 24.8433C65.0779 24.8497 65.0588 24.8521 65.0398 24.8503H62.5378C62.5184 24.8519 62.4988 24.8495 62.4803 24.8431C62.4618 24.8368 62.4448 24.8267 62.4305 24.8135C62.4161 24.8002 62.4047 24.7842 62.3969 24.7663C62.3891 24.7484 62.3851 24.7291 62.3851 24.7096V4.36036C62.3851 4.26659 62.3851 4.2197 62.5378 4.2197H65.0281C65.0479 4.21591 65.0684 4.21683 65.0878 4.22237C65.1072 4.22791 65.1251 4.23792 65.14 4.2516C65.1548 4.26528 65.1662 4.28225 65.1733 4.30114C65.1804 4.32003 65.183 4.34031 65.1808 4.36036Z" fill="black"/>
+<path d="M77.0094 22.5061V24.7098C77.0094 24.8036 77.0094 24.8504 76.8567 24.8504H74.6013C72.0406 24.9052 70.7563 23.733 70.7485 21.3339V12.3783C70.7501 12.3589 70.7477 12.3393 70.7414 12.3209C70.735 12.3025 70.7249 12.2855 70.7116 12.2712C70.6984 12.2569 70.6823 12.2455 70.6644 12.2377C70.6464 12.2299 70.6271 12.2259 70.6076 12.226H67.9881C67.9691 12.2278 67.95 12.2254 67.9321 12.219C67.9142 12.2125 67.8979 12.2022 67.8844 12.1888C67.8709 12.1753 67.8606 12.1591 67.8542 12.1412C67.8477 12.1233 67.8453 12.1042 67.8471 12.0853V10.1746C67.8453 10.1557 67.8477 10.1366 67.8542 10.1187C67.8606 10.1008 67.8709 10.0846 67.8844 10.0711C67.8979 10.0577 67.9142 10.0474 67.9321 10.041C67.95 10.0345 67.9691 10.0321 67.9881 10.034H70.5958C70.6898 10.034 70.7368 10.034 70.7368 9.88157V5.93129C70.7368 5.83752 70.7368 5.79063 70.8895 5.79063H73.3797C73.3987 5.78881 73.4178 5.7912 73.4357 5.79763C73.4537 5.80406 73.47 5.81436 73.4834 5.8278C73.4969 5.84125 73.5072 5.8575 73.5137 5.87539C73.5201 5.89328 73.5225 5.91237 73.5207 5.93129V9.88157C73.519 9.90101 73.5215 9.92057 73.5278 9.93901C73.5342 9.95745 73.5443 9.97438 73.5576 9.98871C73.5708 10.003 73.5869 10.0145 73.6048 10.0222C73.6227 10.03 73.6421 10.034 73.6616 10.034H76.8332C76.8526 10.0323 76.8722 10.0348 76.8907 10.0411C76.9092 10.0474 76.9262 10.0575 76.9405 10.0708C76.9549 10.084 76.9663 10.1001 76.9741 10.1179C76.9819 10.1358 76.9859 10.1551 76.9859 10.1746V12.0736C76.9859 12.1673 76.9859 12.2142 76.8332 12.2142H73.6616C73.5677 12.2142 73.5207 12.2142 73.5207 12.3666V20.9119C73.5207 21.8887 74.0493 22.3771 75.1064 22.3771H76.8214C76.9624 22.3654 77.0094 22.4123 77.0094 22.5061Z" fill="black"/>
+<path d="M83.094 4.36049V7.43163C83.094 7.52541 83.094 7.58402 82.9413 7.58402H79.805C79.711 7.58402 79.664 7.58402 79.664 7.43163V4.36049C79.6622 4.34157 79.6646 4.32248 79.6711 4.30459C79.6775 4.28669 79.6878 4.27045 79.7013 4.25701C79.7148 4.24356 79.7311 4.23326 79.749 4.22683C79.7669 4.2204 79.786 4.21801 79.805 4.21983H82.8826C83.0353 4.21983 83.094 4.26671 83.094 4.36049ZM82.7886 10.2214V24.7566C82.7904 24.7755 82.788 24.7946 82.7816 24.8125C82.7751 24.8304 82.7648 24.8467 82.7514 24.8601C82.7379 24.8735 82.7216 24.8839 82.7037 24.8903C82.6857 24.8967 82.6666 24.8991 82.6476 24.8973H80.1339C80.1149 24.8991 80.0958 24.8967 80.0779 24.8903C80.06 24.8839 80.0437 24.8735 80.0302 24.8601C80.0167 24.8467 80.0064 24.8304 80 24.8125C79.9935 24.7946 79.9911 24.7755 79.993 24.7566V10.1746C79.9911 10.1556 79.9935 10.1365 80 10.1187C80.0064 10.1008 80.0167 10.0845 80.0302 10.0711C80.0437 10.0576 80.06 10.0473 80.0779 10.0409C80.0958 10.0345 80.1149 10.0321 80.1339 10.0339H82.6476C82.6666 10.0321 82.6857 10.0345 82.7037 10.0409C82.7216 10.0473 82.7379 10.0576 82.7514 10.0711C82.7648 10.0845 82.7751 10.1008 82.7816 10.1187C82.788 10.1365 82.7904 10.1556 82.7886 10.1746V10.2214Z" fill="black"/>
+<path d="M86.9702 10.0339H89.202C89.202 10.0339 89.3077 10.0339 89.343 10.1745L89.6484 11.4874C89.6565 11.5215 89.6761 11.5517 89.7039 11.5731C89.7317 11.5944 89.766 11.6056 89.8011 11.6046C89.8352 11.6083 89.8696 11.6026 89.9006 11.5881C89.9317 11.5736 89.9582 11.551 89.9773 11.5226C90.4153 10.926 90.9968 10.4491 91.668 10.1357C92.3393 9.82229 93.0787 9.68247 93.8184 9.72911C94.6976 9.68122 95.5764 9.83123 96.3897 10.168C97.203 10.5049 97.93 11.0199 98.5169 11.675C99.6711 13.0732 100.269 14.8466 100.197 16.6568V24.7097C100.197 24.8035 100.197 24.8504 100.044 24.8504H97.542C97.523 24.8522 97.5039 24.8498 97.486 24.8434C97.468 24.8369 97.4517 24.8266 97.4383 24.8132C97.4248 24.7998 97.4145 24.7835 97.408 24.7656C97.4016 24.7477 97.3992 24.7286 97.401 24.7097V16.6568C97.4394 15.5298 97.1141 14.4204 96.4731 13.4918C96.1425 13.0634 95.7114 12.7227 95.2177 12.4998C94.724 12.277 94.1829 12.1789 93.6422 12.2142C93.0831 12.187 92.5255 12.2905 92.0136 12.5164C91.5017 12.7423 91.0498 13.0843 90.6938 13.5153C89.9829 14.4016 89.6085 15.5101 89.6366 16.645V24.698C89.6385 24.7169 89.6361 24.736 89.6296 24.7539C89.6232 24.7718 89.6129 24.788 89.5994 24.8015C89.5859 24.8149 89.5696 24.8252 89.5517 24.8316C89.5338 24.8381 89.5146 24.8405 89.4957 24.8387H86.9819C86.963 24.8405 86.9439 24.8381 86.9259 24.8316C86.908 24.8252 86.8917 24.8149 86.8782 24.8015C86.8648 24.788 86.8544 24.7718 86.848 24.7539C86.8416 24.736 86.8392 24.7169 86.841 24.698V10.1745C86.8393 10.1567 86.8415 10.1386 86.8472 10.1216C86.853 10.1046 86.8622 10.0889 86.8744 10.0757C86.8866 10.0625 86.9014 10.0519 86.9179 10.0447C86.9344 10.0375 86.9522 10.0338 86.9702 10.0339Z" fill="black"/>
+<path d="M103.791 10.0339H105.999C105.999 10.0339 106.105 10.0339 106.14 10.1745L106.446 11.4874C106.454 11.5215 106.474 11.5517 106.501 11.5731C106.529 11.5944 106.563 11.6056 106.599 11.6046C106.633 11.6083 106.667 11.6026 106.698 11.5881C106.729 11.5736 106.756 11.551 106.775 11.5226C107.213 10.926 107.794 10.4491 108.465 10.1357C109.137 9.82229 109.876 9.68247 110.616 9.72911C111.495 9.68122 112.374 9.83123 113.187 10.168C114 10.5049 114.727 11.0199 115.314 11.675C116.469 13.0732 117.067 14.8466 116.994 16.6568V24.7097C116.994 24.8035 116.994 24.8504 116.841 24.8504H114.351C114.332 24.8522 114.313 24.8498 114.295 24.8434C114.277 24.8369 114.261 24.8266 114.247 24.8132C114.234 24.7998 114.224 24.7835 114.217 24.7656C114.211 24.7477 114.208 24.7286 114.21 24.7097V16.6568C114.249 15.5298 113.923 14.4204 113.282 13.4918C112.952 13.0634 112.521 12.7227 112.027 12.4998C111.533 12.277 110.992 12.1789 110.451 12.2142C109.892 12.187 109.335 12.2905 108.823 12.5164C108.311 12.7423 107.859 13.0843 107.503 13.5153C106.792 14.4016 106.418 15.5101 106.446 16.645V24.698C106.448 24.7169 106.445 24.736 106.439 24.7539C106.432 24.7718 106.422 24.788 106.409 24.8015C106.395 24.8149 106.379 24.8252 106.361 24.8316C106.343 24.8381 106.324 24.8405 106.305 24.8387H103.791C103.772 24.8405 103.753 24.8381 103.735 24.8316C103.717 24.8252 103.701 24.8149 103.687 24.8015C103.674 24.788 103.664 24.7718 103.657 24.7539C103.651 24.736 103.648 24.7169 103.65 24.698V10.1745C103.648 10.1556 103.651 10.1365 103.657 10.1186C103.664 10.1008 103.674 10.0845 103.687 10.0711C103.701 10.0576 103.717 10.0473 103.735 10.0409C103.753 10.0345 103.772 10.0321 103.791 10.0339Z" fill="black"/>
+</svg>

astro-infoportal/public/favicon.svg
@@ -1,9 +1,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 128 128">
-    <path d="M50.4 78.5a75.1 75.1 0 0 0-28.5 6.9l24.2-65.7c.7-2 1.9-3.2 3.4-3.2h29c1.5 0 2.7 1.2 3.4 3.2l24.2 65.7s-11.6-7-28.5-7L67 45.5c-.4-1.7-1.6-2.8-2.9-2.8-1.3 0-2.5 1.1-2.9 2.7L50.4 78.5Zm-1.1 28.2Zm-4.2-20.2c-2 6.6-.6 15.8 4.2 20.2a17.5 17.5 0 0 1 .2-.7 5.5 5.5 0 0 1 5.7-4.5c2.8.1 4.3 1.5 4.7 4.7.2 1.1.2 2.3.2 3.5v.4c0 2.7.7 5.2 2.2 7.4a13 13 0 0 0 5.7 4.9v-.3l-.2-.3c-1.8-5.6-.5-9.5 4.4-12.8l1.5-1a73 73 0 0 0 3.2-2.2 16 16 0 0 0 6.8-11.4c.3-2 .1-4-.6-6l-.8.6-1.6 1a37 37 0 0 1-22.4 2.7c-5-.7-9.7-2-13.2-6.2Z" />
-    <style>
-        path { fill: #000; }
-        @media (prefers-color-scheme: dark) {
-            path { fill: #FFF; }
-        }
-    </style>
-</svg>

astro-infoportal/src/Components/ContactFormModal/ContactFormModal.tsx
@@ -44,6 +44,18 @@ const getLabelValue = (
   return labels?.[key] ?? localization(`/contactform/${key.toLowerCase()}`);
 };
 
+const setRecaptchaBadgeVisibility = (isVisible: boolean) => {
+  if (typeof document === 'undefined') return;
+
+  const badges = document.querySelectorAll<HTMLElement>('.grecaptcha-badge');
+
+  badges.forEach((badge) => {
+    badge.style.visibility = isVisible ? 'visible' : 'hidden';
+    badge.style.opacity = isVisible ? '1' : '0';
+    badge.style.pointerEvents = isVisible ? 'auto' : 'none';
+  });
+};
+
 const ContactFormModal = ({
   isOpen,
   onClose,
@@ -80,6 +92,16 @@ const ContactFormModal = ({
       }
     };
   }, [isOpen, useRecaptcha, recaptchaSiteKey]);
+
+  useEffect(() => {
+    if (!useRecaptcha) return;
+
+    setRecaptchaBadgeVisibility(isOpen);
+
+    return () => {
+      setRecaptchaBadgeVisibility(false);
+    };
+  }, [isOpen, useRecaptcha]);
 
   useEffect(() => {
     if (!isOpen || !modalRef.current) return;

astro-infoportal/src/Constants/routeOverrides.ts
@@ -1,0 +1,29 @@
+const ROUTE_OVERRIDES: Record<string, string> = {
+  "/starte-og-drive/dokumentmaler/": "/starte-og-drive/dokumentmaler/last-ned-dokumentmaler/",
+  "/starte-og-drive/sideblokker/dokumentmaler/": "/starte-og-drive/dokumentmaler/last-ned-dokumentmaler/",
+};
+
+function normalizePath(path?: string) {
+  if (!path || !path.startsWith("/")) {
+    return path;
+  }
+
+  const [pathname, search = ""] = path.split("?", 2);
+  const normalizedPathname = pathname.endsWith("/") ? pathname : `${pathname}/`;
+  return search ? `${normalizedPathname}?${search}` : normalizedPathname;
+}
+
+export function resolveRouteOverride(path?: string) {
+  const normalizedPath = normalizePath(path);
+  if (!normalizedPath) {
+    return undefined;
+  }
+
+  const [pathname, search = ""] = normalizedPath.split("?", 2);
+  const override = ROUTE_OVERRIDES[pathname];
+  if (!override) {
+    return path;
+  }
+
+  return search ? `${override}?${search}` : override;
+}

commit 3775b4497c077b93e9e8efe788df13229226cd7a

Bugfix/urlblock mainbody rendering (#181)
* fix: render inline UrlBlocks inside HeroArticlePageBase mainBody

toRichTextArea collapsed every rich-text item to {componentName, html},
dropping the linkText/url/openInNewWindow props that
RichTextPropertyConverter sets on inline picker blocks. UrlBlock
short-circuited at its !url || !linkText guard and rendered nothing.

Preserve all item props for non-RichText components; keep the
/dokumentmaler/ html rewrite on RichText fragments. Affects articlePage,
sectionArticlePage, newsArticlePage.

TableBlock now also receives its real rows prop, which the Delivery API
returns as "" (empty string) when no rows are configured. rows?.map only
short-circuits on null/undefined, so "" passed through and threw "is not
a function". Guard with Array.isArray.

Regression introduced in #160 (a560960).

* fix: render inline blocks inside SchemaPage mainIntro

Same bug class as the HeroArticlePageBase fix one commit earlier:
SchemaPageTransformer was hard-coding componentName: "RichText" for
every mainIntro item and dropping all props except html. Inline
picker blocks (UrlBlock, LinkBlock, TableBlock, ContentBlock) in a
schemaPage's mainIntro therefore rendered as empty <RichText> divs.

Spread each item, fall back to "RichText" only when componentName
is missing. Also drops the dead translatedHeading: item.name —
neither RichText nor inline-block items carry a name field, and
RichText.tsx doesn't read translatedHeading.

Bug originated in PR #54 (559e97c, 2026-04-07) — predates the
HeroArticle regression in PR #160. No schemaPage on at22 currently
uses inline blocks in mainIntro, so the bug has been latent.

---------

Co-authored-by: Quan Vu <quan.vu@knowit.no>

astro-infoportal/src/Components/Blocks/TableBlock/TableBlock.tsx
@@ -32,7 +32,7 @@ const TableBlock = ({
           </tr>
         </thead>
         <tbody>
-          {rows?.map((row: any, rowIndex: number) => {
+          {Array.isArray(rows) && rows.map((row: any, rowIndex: number) => {
             // Build array of column values in the same order as headers
             const columnValues = [
               row.column1,

astro-infoportal/src/transformers/HeroArticlePageBaseTransformer.ts
@@ -31,10 +31,17 @@ function toRichTextArea(value: any) {
   }
 
   return {
-    items: value.items.map((item: any) => ({
-      componentName: item.componentName ?? "RichText",
-      html: normalizeRichTextHtml(item.html),
-    })),
+    items: value.items.map((item: any) => {
+      const componentName = item.componentName ?? "RichText";
+      if (componentName === "RichText") {
+        return {
+          ...item,
+          componentName,
+          html: normalizeRichTextHtml(item.html),
+        };
+      }
+      return { ...item, componentName };
+    }),
   };
 }
 

astro-infoportal/src/transformers/SchemaPageTransformer.ts
@@ -26,9 +26,8 @@ export class SchemaPageTransformer implements IJSONTransformer {
     const mainBody = props.mainIntro?.items?.length
       ? {
           items: props.mainIntro.items.map((item: any) => ({
-            translatedHeading: item.name,
-            html: item.html,
-            componentName: "RichText",
+            ...item,
+            componentName: item.componentName ?? "RichText",
           })),
         }
       : props.mainIntro || undefined;