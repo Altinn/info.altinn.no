f31be11c635caf127880e0be25fa3b717231b375
Bengt <32734566+bengtfredh@users.noreply.github.com>
2026-06-01 11:37:55 +0200

    feat: Add Kinorgeportal Elasticsearch TT02 infrastructure (#317)
    Adds Terraform configuration for Kinorgeportal Elasticsearch in the
    TT02 test environment, including Azure Key Vault for secrets storage,
    Elastic Cloud Elasticsearch deployment, and GitHub Actions CI/CD
    workflow for automated plan and apply operations.
    
    Signed-off-by: Bengt Fredh <32734566+bengtfredh@users.noreply.github.com>

---
+++ infrastructure/altinn-portaler-test/kinorgeportal-elasticsearch-tt02/main.tf
@@ -0,0 +1,34 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "main" {
+  name     = "kinorgeportal-elasticsearch-tt02"
+  location = "Germany West Central"
+  tags     = local.tags
+}
+
+locals {
+  tags = {
+    finops_environment = "tt02"
+    finops_product     = "kinorgeportal"
+    repository         = "https://github.com/Altinn/info.altinn.no"
+    env                = "tt02"
+    product            = "kinorgeportal"
+  }
+}
+
+module "elasticsearch" {
+  source = "../../modules/kinorgeportal-elasticsearch"
+
+  environment                 = "tt02"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  terraform_object_id         = data.azurerm_client_config.current.object_id
+  eso_object_id               = var.eso_object_id
+  elastic_cloud_api_key       = var.elastic_cloud_api_key
+  elastic_cloud_email_address = var.elastic_cloud_email_address
+  sku_name                    = var.sku_name
+  elasticsearch_api_key       = var.elasticsearch_api_key
+  allowed_cidr_blocks         = var.allowed_cidr_blocks
+  tags                        = local.tags
+}
---
+++ infrastructure/modules/kinorgeportal-elasticsearch/main.tf
@@ -0,0 +1,76 @@
+resource "random_string" "keyvault_suffix" {
+  length  = 4
+  upper   = false
+  special = false
+}
+
+resource "azurerm_key_vault" "elasticsearch" {
+  name                       = "kinorgeportal-es-${var.environment}-${random_string.keyvault_suffix.result}"
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  tenant_id                  = var.tenant_id
+  sku_name                   = "standard"
+  rbac_authorization_enabled = true
+  tags                       = var.tags
+}
+
+resource "azurerm_role_assignment" "terraform_keyvault_officer" {
+  scope                = azurerm_key_vault.elasticsearch.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = var.terraform_object_id
+}
+
+resource "azurerm_role_assignment" "eso_keyvault_reader" {
+  count                = var.eso_object_id != "" ? 1 : 0
+  scope                = azurerm_key_vault.elasticsearch.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = var.eso_object_id
+}
+
+resource "azurerm_elastic_cloud_elasticsearch" "search" {
+  name                        = "kinorgeportal-elasticsearch-${var.environment}"
+  resource_group_name         = var.resource_group_name
+  location                    = var.location
+  sku_name                    = var.sku_name
+  elastic_cloud_email_address = var.elastic_cloud_email_address
+  tags                        = var.tags
+
+  logs {
+    send_activity_logs     = false
+    send_azuread_logs      = false
+    send_subscription_logs = false
+  }
+}
+
+resource "ec_serverless_traffic_filter" "allowed_sources" {
+  count  = length(var.allowed_cidr_blocks) > 0 ? 1 : 0
+  name   = "kinorgeportal-${var.environment}-allowed-sources"
+  region = "azure-germanywestcentral"
+  type   = "ip"
+
+  rules = [for cidr in var.allowed_cidr_blocks : { source = cidr }]
+}
+
+resource "ec_elasticsearch_project" "search" {
+  name               = "kinorgeportal-elasticsearch-${var.environment}"
+  region_id          = "azure-germanywestcentral"
+  traffic_filter_ids = length(var.allowed_cidr_blocks) > 0 ? [ec_serverless_traffic_filter.allowed_sources[0].id] : []
+}
+
+resource "azurerm_key_vault_secret" "es_endpoint" {
+  name         = "elasticsearch-endpoint"
+  value        = ec_elasticsearch_project.search.endpoints.elasticsearch
+  key_vault_id = azurerm_key_vault.elasticsearch.id
+  tags         = var.tags
+
+  depends_on = [azurerm_role_assignment.terraform_keyvault_officer]
+}
+
+resource "azurerm_key_vault_secret" "es_api_key" {
+  name         = "elasticsearch-api-key"
+  value        = var.elasticsearch_api_key
+  key_vault_id = azurerm_key_vault.elasticsearch.id
+  tags         = var.tags
+
+  depends_on = [azurerm_role_assignment.terraform_keyvault_officer]
+}
