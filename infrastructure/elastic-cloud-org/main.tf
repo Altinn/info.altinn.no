9ae6898a1d9683d9ca5592d07788676e88fb8614
Bengt Fredh <32734566+bengtfredh@users.noreply.github.com>
2026-06-02 11:53:58 +0200

    chore: Add Benjamin Brodie to Elastic Cloud org
    Signed-off-by: Bengt Fredh <32734566+bengtfredh@users.noreply.github.com>

--- infrastructure/elastic-cloud-org/main.tf
+++ infrastructure/elastic-cloud-org/main.tf
@@ -6,6 +6,15 @@ resource "ec_organization" "main" {
     "alv.gullbrand.lia@digdir.no" = {
       organization_role = "organization-admin"
     }
+    "benjamin.brodie@digdir.no" = {
+      organization_role = "none"
+      project_elasticsearch_roles = [
+        {
+          project_ids = ["a61b244f20b14002a0bf4f35f0d643bc"]
+          role        = "admin"
+        }
+      ]
+    }
     "vu.quan@digdir.no" = {
       organization_role = "organization-admin"
     }
